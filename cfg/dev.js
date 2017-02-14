4b383d1d5e92e6136201a4f9dd071ce60b8affe1
= <=>
2017-02-13 21:31:50 -0700

    First commit

---
+++ cfg/dev.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+let baseConfig = require('./base');
+let defaultSettings = require('./defaults');
+
+// Add needed plugins here
+let BowerWebpackPlugin = require('bower-webpack-plugin');
+
+let config = Object.assign({}, baseConfig, {
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  cache: true,
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new BowerWebpackPlugin({
+      searchResolveModulesDirectories: false
+    }),
+    new webpack.DefinePlugin({
+     API_SEARCH_HOST: JSON.stringify('http://dev-api-search.plantwithbloom.com:3000'),
+     API_USER_HOST: JSON.stringify('http://dev-api-user.plantwithbloom.com:3001')
+    })
+  ],
+  module: defaultSettings.getDefaultModules()
+});
+
+// Add needed loaders to the defaults here
+config.module.loaders.push({
+  test: /\.(js|jsx)$/,
+  loader: 'react-hot!babel-loader',
+  include: [].concat(
+    config.additionalPaths,
+    [ path.join(__dirname, '/../src') ]
+  )
+});
+
+module.exports = config;
