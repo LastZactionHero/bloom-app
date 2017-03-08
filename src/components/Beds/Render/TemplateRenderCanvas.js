b39b950b88abc3a1e1c8b1f25c738d482b6e1da2
Zach Dicklin <zdicklin@gmail.com>
2017-03-07 20:07:09 -0700

    Color rendering

--- src/components/Beds/Render/TemplateRenderCanvas.js
+++ src/components/Beds/Render/TemplateRenderCanvas.js
@@ -21,27 +21,52 @@ class TemplateRenderCanvas extends React.Component {
     let yImageScale = this.props.renderHeight / this.props.placementHeight;
     let imageScale = Math.min(xImageScale, yImageScale);
 
+    const lineWidth = 1;
+    const colorBrown = '#f39c12';
+    const colorGreen = '#27ae60';
+    const colorLightGreen = '#2ecc71';
+    const colorBlack = '#000000';
+    const colorGray = '#7f8c8d';
+
     // Draw Border
-    ctx.strokeStyle = '#000000';
-    ctx.lineWidth = 1;
+    ctx.strokeStyle = colorBlack;
+    ctx.lineWidth = lineWidth;
     ctx.strokeRect(0, 0, imageScale * this.props.placementWidth, imageScale * this.props.placementHeight);
 
-    // Draw Plants
+    ctx.fillStyle = colorBrown;
+    ctx.fillRect(lineWidth, lineWidth, imageScale * this.props.placementWidth - 2 * lineWidth, imageScale * this.props.placementHeight - 2 * lineWidth);
+
+    // Draw plants, layer 1
     this.props.placements.forEach( (placement) => {
       let xPos = placement.x_pos * imageScale;
       let yPos = placement.y_pos * imageScale;
       let plantRadius = placement.plant.width / 2 * imageScale;
 
       // Plant Circle
+      ctx.fillStyle = colorGray;
       ctx.beginPath();
       ctx.arc(xPos, yPos, plantRadius, 0, 2 * Math.PI);
-      ctx.stroke();
+      ctx.fill();
+    });
+
+    // Draw plants, layer 2 and labels
+    this.props.placements.forEach( (placement) => {
+      let xPos = placement.x_pos * imageScale;
+      let yPos = placement.y_pos * imageScale;
+      let plantRadius = placement.plant.width / 2 * imageScale;
+
+      // Plant Circle
+      ctx.fillStyle = colorLightGreen;
+      ctx.beginPath();
+      ctx.arc(xPos, yPos, plantRadius - 1, 0, 2 * Math.PI);
+      ctx.fill();
 
       // Plant Label
-      const fontSizeLabel = this.props.renderFontSizeLabel || 12;
-      ctx.font=`${fontSizeLabel}px Helvetica`;
+      ctx.font="12px Helvetica";
+      ctx.fillStyle = colorBlack;
       ctx.fillText(placement.plant.label,xPos-4, yPos + 2);
     });
+
   }
 
   render() {
