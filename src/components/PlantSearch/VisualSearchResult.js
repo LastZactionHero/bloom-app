f69d0b19709dd3d3079a4f89d4a79d1eeb1db7bc
Zach Dicklin <zdicklin@gmail.com>
2017-03-07 20:34:22 -0700

    Favorites

--- src/components/PlantSearch/VisualSearchResult.js
+++ src/components/PlantSearch/VisualSearchResult.js
@@ -3,10 +3,17 @@ import React from 'react';
 class VisualSearchResult extends React.Component {
   render() {
     return(
-      <div className='visual-search-result col-md-2'
+      <div className={`visual-search-result col-md-2 ${this.props.plant.favorite ? 'favorite' : null}`}
            style={{backgroundImage: `url("${this.props.plant.image_url}")`}}
            onClick={() => { this.props.onSelect(this.props.plant) } }>
-        <div className='plant-name'>{this.props.plant.common_name}</div>
+        <div className='plant-name'>
+          {this.props.plant.common_name}
+          {this.props.plant.favorite ?
+            <div><small>(Staff Pick)</small></div>
+            : null
+          }
+
+        </div>
       </div>
     )
   }
