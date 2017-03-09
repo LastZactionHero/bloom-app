3e256d3fd8ca5f5f6d1cd909dae3d44e95df13a9
Zach Dicklin <zdicklin@gmail.com>
2017-03-08 21:07:51 -0700

    Rendering: Draw clusters individually by label

--- src/components/Beds/Render/TemplateRenderCanvas.js
+++ src/components/Beds/Render/TemplateRenderCanvas.js
@@ -7,11 +7,11 @@ class TemplateRenderCanvas extends React.Component {
 
   componentDidMount = () => {
     setTimeout( () => {
-      this.renderPlacements();
+      this.renderBed();
     })
   }
 
-  renderPlacements = () => {
+  renderBed = () => {
     let ctx = this.refs.canvas.getContext('2d');
 
     // Determine Image Scaling
@@ -20,51 +20,56 @@ class TemplateRenderCanvas extends React.Component {
     let imageScale = Math.min(xImageScale, yImageScale);
 
     const lineWidth = 1;
-    const colorBrown = '#f39c12';
+    const colorBrown = '#e8b76a';
     const colorGreen = '#27ae60';
     const colorLightGreen = '#2ecc71';
     const colorBlack = '#000000';
     const colorGray = '#7f8c8d';
+    const colorDarkGray = '#2c3e50';
 
     // Draw Border
     ctx.strokeStyle = colorBlack;
     ctx.lineWidth = lineWidth;
     ctx.strokeRect(0, 0, imageScale * this.props.placementWidth, imageScale * this.props.placementHeight);
 
-    ctx.fillStyle = colorBrown;
+    ctx.fillStyle = colorBrown ;
     ctx.fillRect(lineWidth, lineWidth, imageScale * this.props.placementWidth - 2 * lineWidth, imageScale * this.props.placementHeight - 2 * lineWidth);
 
-    // Draw plants, layer 1
-    this.props.placements.forEach( (placement) => {
-      let xPos = placement.x_pos * imageScale;
-      let yPos = placement.y_pos * imageScale;
+    const labels = new Set(this.props.placements.map((p) => {return p.plant.label}));
+    labels.forEach( (label) => {
+      const labelPlacements = this.props.placements.filter((p) => {return p.plant.label == label});
+      this.renderPlacements(ctx, imageScale, labelPlacements, colorGray, {radiusAdjust: 0, offsetLeft: 1, offsetTop: -1} );
+      this.renderPlacements(ctx, imageScale, labelPlacements, colorGreen, {} );
+      this.renderPlacements(ctx, imageScale, labelPlacements, colorLightGreen, {radiusAdjust: -2, printLabel: true}  );
+    })
+  }
+
+  renderPlacements = (ctx, imageScale, placements, color, options) => {
+    const radiusAdjust = options.radiusAdjust || 0;
+    const offsetTop = options.offsetTop || 0;
+    const offsetLeft = options.offsetLeft || 0;
+    const printLabel = options.printLabel || false;
+
+    placements.forEach( (placement) => {
+      let xPos = placement.x_pos * imageScale + offsetLeft;
+      let yPos = placement.y_pos * imageScale + offsetTop;
       let plantRadius = placement.plant.width / 2 * imageScale;
 
       // Plant Circle
-      ctx.fillStyle = colorGray;
+      ctx.fillStyle = color;
       ctx.beginPath();
-      ctx.arc(xPos, yPos, plantRadius, 0, 2 * Math.PI);
-      ctx.fill();
-    });
 
-    // Draw plants, layer 2 and labels
-    this.props.placements.forEach( (placement) => {
-      let xPos = placement.x_pos * imageScale;
-      let yPos = placement.y_pos * imageScale;
-      let plantRadius = placement.plant.width / 2 * imageScale;
-
-      // Plant Circle
-      ctx.fillStyle = colorLightGreen;
-      ctx.beginPath();
-      ctx.arc(xPos, yPos, plantRadius - 1, 0, 2 * Math.PI);
+      ctx.arc(xPos, yPos, plantRadius + radiusAdjust, 0, 2 * Math.PI);
       ctx.fill();
 
       // Plant Label
-      ctx.font="12px Helvetica";
-      ctx.fillStyle = colorBlack;
-      ctx.fillText(placement.plant.label,xPos-4, yPos + 2);
+      if(printLabel){
+        ctx.font="12px Helvetica";
+        ctx.fillStyle = '#000000';
+        ctx.textAlign = 'center';
+        ctx.fillText(placement.plant.label,xPos, yPos+3);
+      }
     });
-
   }
 
   render() {
